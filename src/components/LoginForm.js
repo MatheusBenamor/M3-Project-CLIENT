cdad66ca8f784fd457cf90d15b2b042824e64a46
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2022-05-01 12:17:06 +0100

    improve analysis page style

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -48,18 +48,3 @@ const LoginForm = () => {
   };
   
   export default LoginForm;
-
-  /*<form class="login" action="" method="get">
-        
-        <div class="formGroup">
-          <input type="email" placeholder="Email" name="email" required autocomplete="off"></input>
-        </div>
-        <div class="formGroup">
-          <input type="password" id="password" placeholder="Password" required autocomplete="off"></input>
-         
-        </div>
-        <div class="formGroup">
-          <button type="button" class="btn2">Login</button>
-        </div>
- 
-      </form>*/
--- src/pages/Analysis.js
+++ src/pages/Analysis.js
@@ -23,18 +23,20 @@ const Analysis = () => {
     return (
       <>
       <form>
-      <h1> This is my analysis page </h1>
+      <div className='title-container'>
+      <h1><input type="text" placeholder="            Analysis Name" className='input-title'></input></h1>
+      </div>
       <section className='all-blocks'>
 
 <div>
         <div className='color-blue'>
         <h3 className='margin-align'>Strenghts</h3>
         <ul>
-        <li><input type="text" placeholder="Strenght 1" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
-        <li><input type="text" placeholder="Strenght 2" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
-        <li><input type="text" placeholder="Strenght 3" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
-        <li><input type="text" placeholder="Strenght 4" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
-        <li><input type="text" placeholder="Strenght 5" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
+        <li><input type="text" placeholder="Strenghts" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputStrength" ></input></li>
         </ul>
         <div className='flex-end'><StrengthInfo></StrengthInfo></div>
         </div>
@@ -43,11 +45,11 @@ const Analysis = () => {
         <div className='color-yellow'>
         <h3 className='margin-align'>Weaknesses</h3>
         <ul>
-        <li><input type="text" placeholder="Weakness 1" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
-        <li><input type="text" placeholder="Weakness 2" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
-        <li><input type="text" placeholder="Weakness 3" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
-        <li><input type="text" placeholder="Weakness 4" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
-        <li><input type="text" placeholder="Weakness 5" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
+        <li><input type="text" placeholder="Weaknesses" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputWeakness"></input></li>
         </ul>
         <div className='flex-end'><WeaknessInfo></WeaknessInfo></div>
         </div>
@@ -57,11 +59,11 @@ const Analysis = () => {
         <div className='color-green'>
         <h3 className='margin-align'>Opportunities</h3>
         <ul>
-        <li><input type="text" placeholder="Opportunity 1" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
-        <li><input type="text" placeholder="Opportunity 2" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
-        <li><input type="text" placeholder="Opportunity 3" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
-        <li><input type="text" placeholder="Opportunity 4" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
-        <li><input type="text" placeholder="Opportunity 5" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
+        <li><input type="text" placeholder="Opportunities" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputOpportunity"></input></li>
         </ul>
         <div className='flex-end'><OpportunitiesInfo></OpportunitiesInfo></div>
         </div>
@@ -70,11 +72,11 @@ const Analysis = () => {
         <div className='color-red'>
         <h3 className='margin-align'>Threats</h3>
         <ul>
-        <li><input type="text" placeholder="Threat 1" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputThreat"></input></li>
-        <li><input type="text" placeholder="Threat 2" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputThreat"></input></li>
-        <li><input type="text" placeholder="Threat 3" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputThreat"></input></li>
-        <li><input type="text" placeholder="Threat 4" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputThreat"></input></li>
-        <li><input type="text" placeholder="Threat 5" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputThreat"></input></li>
+        <li><input type="text" placeholder="Threats" className='input-texting'></input><input type="Number" placeholder="Relevance" min="1" max="5" className='input-score' id="inputThreat"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputThreat"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputThreat"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputThreat"></input></li>
+        <li><input type="text" className='input-texting'></input><input type="Number" min="1" max="5" className='input-score' id="inputThreat"></input></li>
         </ul>
         <div className='flex-end'><ThreatsInfo></ThreatsInfo></div>
         </div>
@@ -85,7 +87,6 @@ const Analysis = () => {
         <button type='submit' className='analysis-buttom'>SEND ANALYSIS AND CALCULATE RESULTS </button>
         </Link>
         <CalculateAnalysis></CalculateAnalysis>
-        <h4>Total Price: <span id="totalPrice">0</span></h4>
         </form>
       </>
     )
